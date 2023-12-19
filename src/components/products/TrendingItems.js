416aaa9c7b657c417476050198f69e53e7173cb4
Rahul Dhiman <2021mca015@timt.ac.in>
2023-12-19 15:11:52 +0530

    "Trending items crousel modify"

--- src/components/products/TrendingItems.js
+++ src/components/products/TrendingItems.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import Carousel from 'react-bootstrap/Carousel'
 import { useSelector } from 'react-redux'
 import { Container } from 'react-bootstrap';
 import TrendingItemsCard from './TrendingItemsCard';
@@ -8,49 +7,53 @@ import TrendingItemsCard from './TrendingItemsCard';
 function TrendingItems() {
     const product = useSelector(state => state.FilterSort.products)
 
-    const [index, setIndex] = useState(0);
-
-    const handleSelect = (selectedIndex) => {
-        setIndex(selectedIndex);
-    };
-
     return (
         <Container fluid className='border'>
             <p className='display-6 text-center'>Trending Products</p>
-            <Carousel activeIndex={index} onSelect={handleSelect} data-bs-theme="dark">
-                <Carousel.Item>
-                    <Container className='d-flex justify-content-between align-items-center'>
-                        {
-                            product.slice(8, 11).map(item => {
-                                return <TrendingItemsCard
-                                    key={item.id}
-                                    thumbnail={item.thumbnail}
-                                    title={item.title}
-                                    description={item.description}
-                                    price={item.price}
-                                    discount={item.discountPercentage}
-                                />
-                            })
-                        }
-                    </Container>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <Container className='d-flex justify-content-between align-items-center'>
-                        {
-                            product.slice(22, 25).map(item => {
-                                return <TrendingItemsCard
-                                    key={item.id}
-                                    thumbnail={item.thumbnail}
-                                    title={item.title}
-                                    description={item.description}
-                                    price={item.price}
-                                    discount={item.discountPercentage}
-                                />
-                            })
-                        }
-                    </Container>
-                </Carousel.Item>
-            </Carousel>
+            <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <Container className='d-flex justify-content-between align-items-center'>
+                            {
+                                product.slice(8, 11).map(item => {
+                                    return <TrendingItemsCard
+                                        key={item.id}
+                                        thumbnail={item.thumbnail}
+                                        title={item.title}
+                                        description={item.description}
+                                        price={item.price}
+                                        discount={item.discountPercentage}
+                                    />
+                                })
+                            }
+                        </Container>
+                    </div>
+                    <div class="carousel-item">
+                        <Container className='d-flex justify-content-between align-items-center'>
+                            {
+                                product.slice(22, 25).map(item => {
+                                    return <TrendingItemsCard
+                                        key={item.id}
+                                        thumbnail={item.thumbnail}
+                                        title={item.title}
+                                        description={item.description}
+                                        price={item.price}
+                                        discount={item.discountPercentage}
+                                    />
+                                })
+                            }
+                        </Container>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
         </Container>
     )
 }
