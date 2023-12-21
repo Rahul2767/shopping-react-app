5818682dfcd0e8e4992cd8143d28ddf68420ae95
Rahul Dhiman <2021mca015@timt.ac.in>
2023-12-21 13:18:18 +0530

    change server

--- src/features/apiSlice.js
+++ src/features/apiSlice.js
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl : 'https://shopping-react-app-api.vercel.app',
+    baseUrl : 'https://shopping-react-app-backend.onrender.com',
     credentials:"include"
 })
 
