33d4759f1f5bce79095d05d822f0077b4e330eb4
Rahul Dhiman <2021mca015@timt.ac.in>
2024-01-03 10:05:22 +0530

    fcdfcd

--- src/components/profile/ProfileUpdate.js
+++ src/components/profile/ProfileUpdate.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import Form, { Container } from "react-bootstrap";
 
 function ProfileUpdate() {
   return (
-    <p>
-      this is profile update
-    </p>
-  )
+    <Container>
+      <h1 className="display-6 text-center border-bottom">Profile</h1>{" "}
+      <Form>
+        <Form.Group>
+          <Form.Label>Enter your full name:</Form.Label>
+          <Form.Control type="text" placeholder="Enter your full name" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Enter your email address:</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter your your email address"
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Enter your age:</Form.Label>
+          <Form.Control type="number" placeholder="Enter your age" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Click here to submit form
+        </Button>
+      </Form>
+    </Container>
+  );
 }
 
-export default ProfileUpdate
+export default ProfileUpdate;
