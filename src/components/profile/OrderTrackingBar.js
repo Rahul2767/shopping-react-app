d029c78feaa94150c979c23a386fbdde55aa4b3c
Rahul Dhiman <2021mca015@timt.ac.in>
2024-01-08 12:58:01 +0530

    fedfe

--- src/components/profile/OrderTrackingBar.js
+++ src/components/profile/OrderTrackingBar.js
@@ -1,29 +1,26 @@
-import React, { useState } from "react";
-import { ProgressBar } from "react-step-progress-bar";
+import React from "react";
+import "react-step-progress-bar/styles.css";
+import { ProgressBar, Step } from "react-step-progress-bar";
 
 function OrderTrackingBar() {
-  const orderStatus = "Shipped";
-
-  const steps = ["Placed", "Confirmed", "Shipped", "Deliverd"];
-  const labels = {
-    Placed: "Your order has been placed",
-    Confirmed: "You order has been confirmed",
-    Shipped: "Your order has been shipped",
-    Delivered: "Your order has been delivered",
-  };
-  const percent = (steps.indexOf(orderStatus) + 1) * 25;
-
   return (
-    <div className="order-tracking">
-      <h1>Order Tracking</h1>
-      <ProgressBar
-        percent={percent}
-        steps={steps}
-        filledBackground="#00bfff"
-        text={orderStatus}
-      />
-      <p>{labels[orderStatus]}</p>
-    </div>
+    <ProgressBar
+      percent={75}
+      filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
+    >
+      <Step transition="scale">
+        <i class="bi bi-check-circle-fill"></i>
+      </Step>
+      <Step transition="scale">
+        <i class="bi bi-box-seam-fill"></i>
+      </Step>
+      <Step transition="scale">
+        <i class="bi bi-truck"></i>
+      </Step>
+      <Step transition="scale">
+        <i class="bi bi-check2-circle"></i>
+      </Step>
+    </ProgressBar>
   );
 }
 
