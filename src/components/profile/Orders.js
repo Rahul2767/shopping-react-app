3b41dabbf7f783ed6ff0c605f91629d9133492c7
Rahul Dhiman <2021mca015@timt.ac.in>
2024-01-08 11:38:22 +0530

    scscsc

--- src/components/profile/Orders.js
+++ src/components/profile/Orders.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react'
-import { Container } from 'react-bootstrap';
-import { useSelector, useDispatch } from 'react-redux'
-import { useGetOrdersMutation } from '../../features/usersApiSlice';
-import { placedOrders } from '../../features/authSlice';
+import React, { useEffect } from "react";
+import { Container } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { useGetOrdersMutation } from "../../features/usersApiSlice";
+import { placedOrders } from "../../features/authSlice";
+import OrderTrackingBar from "./OrderTrackingBar";
 
 function Orders() {
-  const authState = useSelector((state) => state.auth)
-  const [getOrders, { isLoading }] = useGetOrdersMutation()
-  const dispatch = useDispatch()
+  const authState = useSelector((state) => state.auth);
+  const [getOrders, { isLoading }] = useGetOrdersMutation();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const renderOrders = async () => {
@@ -26,23 +27,39 @@ function Orders() {
     <Container>
       <h1 className="display-6 text-center border-bottom">Orders</h1>
       <Container>
-        {isLoading ? <Container className='d-flex justify-content-center'><div class="spinner-border text-success my-2" role="status">
-          <span class="sr-only"></span>
-        </div></Container>
-          :
-          authState.userOrders.length > 0 ? authState.userOrders.map(order => {
-            return <div className='border mb-2 p-1 d-flex flex-column'>
-              <div className='border-bottom fs-6'><p>Order id - {order._id}</p></div>
-              <div className='d-flex justify-content-between'>
-                <div className='display-6 fs-4'><p>Product - {order.productTitle}</p></div>
-                <div className='fs-5 text-success'><p>Price - ${order.productPrice}</p></div>
-              </div>
+        {isLoading ? (
+          <Container className="d-flex justify-content-center">
+            <div class="spinner-border text-success my-2" role="status">
+              <span class="sr-only"></span>
             </div>
-          }) : <p>You have no orders yet</p>
-        }
+          </Container>
+        ) : authState.userOrders.length > 0 ? (
+          authState.userOrders.map((order) => {
+            return (
+              <div className="border mb-2 p-1 d-flex flex-column">
+                <div className="border-bottom fs-6">
+                  <p>Order id - {order._id}</p>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <div className="display-6 fs-4">
+                    <p>Product - {order.productTitle}</p>
+                  </div>
+                  <div>
+                    <OrderTrackingBar />
+                  </div>
+                  <div className="fs-5 text-success">
+                    <p>Price - ${order.productPrice}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <p>You have no orders yet</p>
+        )}
       </Container>
-    </Container >
-  )
+    </Container>
+  );
 }
 
-export default Orders
+export default Orders;
